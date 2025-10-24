34821830d4ce659c0a6b0a42ee0b5941bd449215
codexpert.tech <codexpertech@gmail.com>
2025-10-24 22:52:23 +0200

    Add Alert, AlertDialog, and Dialog UI components
    Introduced new UI components for Alert, AlertDialog, and Dialog, including their subcomponents and index exports. Updated App.vue to demonstrate usage of these components. Also made minor code style improvements in Button and Button index files.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,109 @@
 <script setup lang="ts">
   import LoginForm from './components/LoginForm.vue';
+  import { Rocket } from 'lucide-vue-next'
+  import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+  import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+
+  import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+
+  import { ref } from 'vue'
+
+  const handleCancel = () => {
+    console.log('Cancel')
+  }
+
+  const handleContinue = () => {
+    console.log('Continue')
+  }
+
+  const open = ref(false)
+
 </script>
 <template>
-  <LoginForm class="m-5" />
+  <!--<LoginForm class="m-5" />-->
+
+  <Alert>
+    <Rocket class="h-4 w-4" />
+    <AlertTitle>Heads up!</AlertTitle>
+    <AlertDescription>
+      You can add components to your app using the cli.
+    </AlertDescription>
+  </Alert>
+
+  <AlertDialog v-model:open=open>
+    <AlertDialogTrigger as-child>
+      <Button variant="default">
+        Show Dialog
+      </Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel @click="handleCancel">Cancel</AlertDialogCancel>
+        <AlertDialogAction @click="handleContinue">Continue</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+
+  <br/>
+  
+  <Dialog>
+    <DialogTrigger as-child>
+      <Button variant="outline">
+        Edit Profile
+      </Button>
+    </DialogTrigger>
+    <DialogContent class="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="name" class="text-right">
+            Name
+          </Label>
+          <Input id="name" value="Pedro Duarte" class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="username" class="text-right">
+            Username
+          </Label>
+          <Input id="username" value="@peduarte" class="col-span-3" />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button type="submit">
+          Save changes
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+
 </template>
